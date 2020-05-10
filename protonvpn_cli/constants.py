9f8a966d412763339c0192439d94218375b98125
Alexandru Cheltuitor <31934100+calexandru2018@users.noreply.github.com>
2020-05-10 17:56:41 +0200

    Added support for PolKit

--- protonvpn_cli/constants.py
+++ protonvpn_cli/constants.py
@@ -1,10 +1,14 @@
 import os
 import getpass
+import pwd
 
 try:
-    USER = os.environ["SUDO_USER"]
+    USER = pwd.getpwuid(int(os.environ["PKEXEC_UID"])).pw_name
 except KeyError:
-    USER = getpass.getuser()
+    try:
+        USER = os.environ["SUDO_USER"]
+    except KeyError:
+        USER = getpass.getuser()
 
 CONFIG_DIR = os.path.join(os.path.expanduser("~{0}".format(USER)), ".pvpn-cli")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "pvpn-cli.cfg")
